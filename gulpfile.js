d9ed0a188b21f9675ac5373e9f39c0569d0eba1b
Dylan Landry <dylanglandry@gmail.com>
2019-02-18 08:27:39 -0500

    add gulp watch scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch, parallel, series } = require('gulp')
 const rimraf = require('rimraf')
 const pug = require('gulp-pug')
+const sass = require('gulp-sass')
 
 const buildDir = `${__dirname}/public`
 const pagesDir = `${__dirname}/pages`
+const stylesDir = `${__dirname}/pages/styles`
 
 function clean (cb) {
   rimraf(`${buildDir}/**`, cb)
@@ -17,10 +19,24 @@ function template () {
 
 function _watch () {
   watch(`${pagesDir}/*.pug`, template)
+  watch(`${stylesDir}/*.scss`, css)
 }
+
+function css () {
+  return src(`${stylesDir}/*.scss`)
+    .pipe(sass().on(`error`, sass.logError))
+    .pipe(dest(`${buildDir}/styles`))
+}
+
+function build(cb) {
+  return series(clean, parallel(css, template))(cb)
+}
+
 
 module.exports = {
   template,
-  watch: _watch,
+  watch: series(build, _watch),
   clean,
+  css,
+  build,
 }
