ba287d8904abb217cfdfa22b5417302fadea0b94
Dylan Landry <dylanglandry@gmail.com>
2019-02-18 07:44:21 -0500

    add gulp build pug template files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
+const { src, dest } = require('gulp')
 const rimraf = require('rimraf')
+const pug = require('gulp-pug')
 
 const buildDir = `${__dirname}/public`
+const pagesDir = `${__dirname}/pages`
 
 exports.clean = function (cb) {
   rimraf(`${buildDir}/**`, cb)
+}
+
+exports.template = function () {
+  return src(`${pagesDir}/*.pug`)
+    .pipe(pug())
+    .pipe(dest(buildDir))
 }
